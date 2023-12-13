8412def82d158a17b439c5030cf265e700bf682e
Artūrs Izkalnis <aizkalnis@edu.riga.lv>
2023-12-13 09:50:55 +0200

    aizkalnis

--- calc.py
+++ calc.py
@@ -1,24 +1,38 @@
 
 # konvertē datu mērvienības
 def megabit_to_megabyte(megabit):
-   result = 0
+   result = megabit * 8
    return result
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
+   result = (litres * 100) / kilometers
    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = celsius * 1.8 + 32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
-    result = 0
+    result=0
+    a=1
+    while (tail>=a):
+        result = result + a
+        a += 1
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
 def weight(grams):
-    return str(grams) + "g"
+    if (grams>=1000000):
+        grams=grams/1000000
+        return str(int(grams)) + "t"
+    elif (grams>=100000):
+        grams=grams/100000
+        return str(int(grams)) + "c"
+    elif (grams>=1000):
+        grams=grams/1000
+        return str(int(grams)) + "kg"
+    else:
+        return str(int(grams)) + "g"
